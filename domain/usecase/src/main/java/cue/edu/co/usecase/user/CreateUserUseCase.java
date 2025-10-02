5c1b9d0593c43c308828eae67d6787ca2e7944e9
Juan Gerardo <jmendez1025@cue.edu.co>
2025-10-01 21:45:05 -0500

    feat(usecase): implement CreateUser use case

---
+++ domain/usecase/src/main/java/cue/edu/co/usecase/user/CreateUserUseCase.java
@@ -0,0 +1,62 @@
+package cue.edu.co.usecase.user;
+
+import cue.edu.co.model.role.Role;
+import cue.edu.co.model.role.exceptions.RoleNotFoundException;
+import cue.edu.co.model.role.gateways.RoleRepository;
+import cue.edu.co.model.security.gateways.PasswordEncoder;
+import cue.edu.co.model.user.User;
+import cue.edu.co.model.user.comnands.CreateUserCommand;
+import cue.edu.co.model.user.exceptions.EmailAlreadyInUseException;
+import cue.edu.co.model.user.exceptions.IdentificationAlreadyInUseException;
+import cue.edu.co.model.user.exceptions.PhoneAlreadyInUseException;
+import cue.edu.co.model.user.gateways.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+
+@RequiredArgsConstructor
+public class CreateUserUseCase {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User execute(CreateUserCommand command) {
+        Role role = validateRoleExists(command.roleId());
+
+        validateUnique(
+                Optional.of(command.email()),
+                userRepository::existsByEmail,
+                new EmailAlreadyInUseException()
+        );
+
+        validateUnique(
+                command.identification(),
+                userRepository::existsByIdentification,
+                new IdentificationAlreadyInUseException()
+        );
+
+        validateUnique(
+                command.phoneNumber(),
+                userRepository::existsByPhoneNumber,
+                new PhoneAlreadyInUseException()
+        );
+
+        User user = command.toDomain(role, passwordEncoder.encode(command.password()));
+
+        return userRepository.save(user);
+    }
+
+
+    private Role validateRoleExists(Long roleId) {
+        return roleRepository.findById(roleId)
+                .orElseThrow(RoleNotFoundException::new);
+    }
+
+    private <T> void validateUnique(Optional<T> value, Predicate<T> existsCheck, RuntimeException ex) {
+        value.ifPresent(v -> {
+            if (existsCheck.test(v)) throw ex;
+        });
+    }
+}
